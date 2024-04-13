213b2b42960862c9ad6a7af52199455b13ddebf9
Mica <mika89700@gmail.com>
2024-04-13 12:54:12 +0200

    typo

--- run.sh
+++ run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# install python
-set +e
-python --version 2> /dev/null
-if [[ ! "$?" -eq 0 ]]; then
-    echo "You need to install python ! See https://www.python.org/downloads/macos/"
-    exit 1
-fi
-set -e
-
 # install ansible
 pip install ansible
 
