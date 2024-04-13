79b38c14651f5382d4ac0f6454abfd519abddca7
Mica <mika89700@gmail.com>
2024-04-13 13:27:14 +0200

    lint

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # install ansible
 pip install ansible
-ansible-galaxy collection install community.general
+ansible-galaxy collection install -r requirements.yml
 
 # run ansible
 ansible-playbook -i host macinstall.yml
